424ee2cbfbb916dadbf85dff86071d609df3b2eb
Gene Wong <gene.h.wong@gmail.com>
2022-04-26 20:44:24 -0700

    changed localhost to DB AWS host

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ const Pool = require('pg').Pool;
 const pool = new Pool({
   user: process.env.DB_USER || 'postgres',
   password: process.env.DB_PW || 'endorphins',
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || '3.101.61.193',
   port: process.env.DB_PORT || '5432',
   database: process.env.DB_NAME || 'sdc_lw'
 });
